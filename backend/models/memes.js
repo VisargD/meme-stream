de0f7d8b42f4ef83f95a4c6a6cd70155923edf34
VisargD <visargdesai@gmail.com>
2021-02-06 00:43:35 +0530

    Add Schema for MongoDB collection

---
+++ backend/models/memes.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const memeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    caption: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: Date
+    },   
+})
+
+memeSchema.set('toJSON', {
+  virtuals: true,
+  versionKey:false,
+  transform: function (doc, ret) { 
+      ret.id = ret._id;
+      delete ret.__v;  
+      delete ret._id;  
+    }
+});
+
+const Meme = mongoose.model('Meme', memeSchema);
+
+module.exports = Meme;
