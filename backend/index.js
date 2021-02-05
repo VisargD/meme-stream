b3ac1309e4de70948e183be9a162bf7904019f2f
VisargD <visargdesai@gmail.com>
2021-02-06 00:13:00 +0530

    Add mongoose connection code

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/xmeme', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('MongoDB connection successfull');
+    })
+    .catch(err => {
+        console.log('Error connecting to MongoDB');
+        console.log(err);
+    })
 
 
 app.listen(8081, () => {
