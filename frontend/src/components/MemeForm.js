417807d4c3d4db8911a5ee3a1181e601912c1a05
visargD <visargdesai@gmail.com>
2021-02-07 23:55:40 +0530

    Add Material UI to Form Component

--- frontend/src/components/MemeForm.js
+++ frontend/src/components/MemeForm.js
@@ -2,6 +2,19 @@ import React, { useContext } from "react";
 import axios from "axios";
 import { useState } from "react";
 import { MemeContext } from "../context/MemeContext";
+import { TextField, Button, makeStyles, Grid } from "@material-ui/core";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    "& .MuiFormControl-root": {
+      margin: theme.spacing(1),
+      width: "50%",
+    },
+    "& Button": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 export default function MemeForm() {
   const [memes, setMemes] = useContext(MemeContext);
@@ -23,34 +36,46 @@ export default function MemeForm() {
     }
   };
 
+  const classes = useStyle();
+
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <input
-          class="inputField"
-          name="name"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <input
-          class="inputField"
-          name="url"
-          placeholder="url"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          required
-        />
-        <input
-          class="inputField"
-          name="caption"
-          placeholder="Caption"
-          value={caption}
-          onChange={(e) => setCaption(e.target.value)}
-          required
-        />
-        <button class="inputField">Submit</button>
+      <form className={classes.root} onSubmit={submitHandler}>
+        <Grid>
+          <TextField
+            id="outlined-basic"
+            name="name"
+            label="Name"
+            variant="outlined"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+
+          <TextField
+            id="outlined-basic"
+            name="url"
+            label="URL"
+            variant="outlined"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            required
+          />
+
+          <TextField
+            id="outlined-basic"
+            name="caption"
+            label="Caption"
+            variant="outlined"
+            value={caption}
+            onChange={(e) => setCaption(e.target.value)}
+            required
+          />
+        </Grid>
+
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
       </form>
     </div>
   );
--- frontend/src/components/MemeList.js
+++ frontend/src/components/MemeList.js
@@ -2,16 +2,17 @@ import React, { useContext } from "react";
 import MemeForm from "./MemeForm";
 import { MemeContext } from "../context/MemeContext";
 import axios from "axios";
+import { Button } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 export default function MemeList() {
   const [memes, setMemes] = useContext(MemeContext);
 
-
-  const deleteHandler = async (id) => {    
-    await axios.delete('/memes/' + id);
-    const data = await axios.get('/memes');
-    setMemes(data.data);    
-  }
+  const deleteHandler = async (id) => {
+    await axios.delete("/memes/" + id);
+    const data = await axios.get("/memes");
+    setMemes(data.data);
+  };
   return (
     <div>
       <MemeForm />
@@ -21,7 +22,14 @@ export default function MemeList() {
             <h2>{meme.name}</h2>
             <h3>{meme.caption}</h3>
             <img class="meme-image" src={meme.url} />
-            <button onClick={() => deleteHandler(meme.id)}>Delete</button>
+            <Button
+              variant="contained"
+              color="secondary"
+              startIcon={<DeleteIcon />}
+              onClick={() => deleteHandler(meme.id)}
+            >
+              Delete
+            </Button>
           </>
         );
       })}
