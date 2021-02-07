99aab788ad06ad0745ffd97cf8c35b434397fc54
visargD <visargdesai@gmail.com>
2021-02-07 17:37:40 +0530

    Add Delete functionality in frontend

--- frontend/src/components/MemeList.js
+++ frontend/src/components/MemeList.js
@@ -1,10 +1,17 @@
 import React, { useContext } from "react";
 import MemeForm from "./MemeForm";
 import { MemeContext } from "../context/MemeContext";
+import axios from "axios";
 
 export default function MemeList() {
   const [memes, setMemes] = useContext(MemeContext);
 
+
+  const deleteHandler = async (id) => {    
+    await axios.delete('/memes/' + id);
+    const data = await axios.get('/memes');
+    setMemes(data.data);    
+  }
   return (
     <div>
       <MemeForm />
@@ -14,6 +21,7 @@ export default function MemeList() {
             <h2>{meme.name}</h2>
             <h3>{meme.caption}</h3>
             <img class="meme-image" src={meme.url} />
+            <button onClick={() => deleteHandler(meme.id)}>Delete</button>
           </>
         );
       })}
