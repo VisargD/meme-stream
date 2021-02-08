0b88ff64e7656439553f9cfe6a6b3d586247dc4b
visargD <visargdesai@gmail.com>
2021-02-08 21:23:35 +0530

    Modify MongoDb Schema with likes and dislikes

--- backend/models/memes.js
+++ backend/models/memes.js
@@ -1,34 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const memeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    url: {
-        type: String,
-        required: true,
-    },
-    caption: {
-        type: String,
-        required: true,
-    },
-    timestamp: {
-        type: Date
-    },   
-})
-
-memeSchema.set('toJSON', {
-  virtuals: true,
-  versionKey:false,
-  transform: function (doc, ret) { 
-      ret.id = ret._id;
-      delete ret.__v;  
-      delete ret._id; 
-      delete ret.timestamp; 
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+  },
+  likes: {
+    type: Array,
+  },
+  dislikes: {
+    type: Array,
+  },
 });
 
-const Meme = mongoose.model('Meme', memeSchema);
+memeSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret.__v;
+    delete ret._id;
+    delete ret.timestamp;
+    delete ret.likes;
+    delete ret.dislikes;
+  },
+});
+
+const Meme = mongoose.model("Meme", memeSchema);
 
 module.exports = Meme;
