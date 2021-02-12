47aa93646eefb1f9ac16467d97947a600164a247
visargD <visargdesai@gmail.com>
2021-02-12 18:42:04 +0530

    Add default axios baseUrl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import MemeList from "./components/home/MemeList";
 import { MemeProvider } from "./context/MemeContext";
+import axios from 'axios';
+
+axios.defaults.baseUrl = "http://localhost:8081";
 
 function App() {
   return (
